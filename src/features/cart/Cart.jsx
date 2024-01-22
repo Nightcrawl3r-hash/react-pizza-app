d0b3c4529f1b30eb544197a35104d9c3bf5b2681
Naresh Rajbanshi <nar3sh0@gmail.com>
2024-01-22 20:39:02 +0545

    redux for user done

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const fakeCart = [
   {
@@ -27,26 +28,25 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector((state) => state.user.userName);
   const cart = fakeCart;
 
   return (
     <div className="px-4 py-3 ">
       <Link to="/menu">&larr; Back to menu</Link>
 
-      <h2 className="mt-7 text-xl  font-semibold">Your cart, %NAME%</h2>
-      <ul className="divide-y divide-stone-200  border-b mt-3">
+      <h2 className="mt-7 text-xl  font-semibold">Your cart, {username}</h2>
+      <ul className="mt-3 divide-y  divide-stone-200 border-b">
         {cart.map((item) => (
           <CartItem item={item} key={item.key} />
         ))}
       </ul>
-      <div className='mt-6 space-x-2'>
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order Pizzas
         </Button>
 
-
-
-       <Button type='secondary'>Clear cart</Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Username() {
+  const username = useSelector((state) => state.user.userName);
+  if (!username) return null;
   return (
-    <div className='text-sm font-semibold hidden md:block'>Naresh</div>
-  )
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
-export default Username
+export default Username;
